4aee754cd6cdc46498255c7e2586b6e2355986f4
DavidFrenchSG <David.French@gov.scot>
2023-11-08 19:01:22 +0000

    Commenting code

--- Minor_holdings.R
+++ Minor_holdings.R
@@ -4,11 +4,11 @@ library(haven)
 library(data.table)
 library(writexl)
 
-
+#Years of interest. Would ideally extend to 2022 and 2023, but unsure of the right datasets to use.
 year_range = 2020:2021
 census_directory_path <- '//s0177a/sasdata1/ags/census/agscens/'
 
-#Function to check if a dataset exists in the working directory, then either import it if so, or download it then import it.
+#Function to check if a dataset exists in the working directory, then either read it if so, or download it then read it.
 read_sas2 <- function(directory, filename){
   census_data <- tryCatch(
     {
@@ -36,24 +36,28 @@ all_years <- NULL
 for (year in year_range){
     census_data_file <- paste0("june", year-2000, ".sas7bdat")
     single_year <- import_sas(census_directory_path, census_data_file) %>% 
+      #total_fruit = soft fruit + orchard fruit
       mutate(total_fruit = item36 + item37,
+             #total grass = grass under 5 years + grass over 5 years
              total_grass = item2321+item2322,
              cropyear = year) %>% 
       select(parish, holding, cropyear,
-             total_crops_and_fallow = item40,
              total_fruit,
+             total_grass,
+             total_crops_and_fallow = item40,
              total_ornamentals = item82,
              total_glasshouse = item1943,
-             total_grass,
-             rough_grazing = item47,
-             cgarea) %>% 
+             rough_grazing = item47) %>% 
       rowwise() %>% 
+      #ornamentals appear to be included in total_crops_and_fallow already
       mutate(total_relevant_area = sum(total_crops_and_fallow, total_fruit, total_glasshouse, total_grass, na.rm=T)) %>% 
+      #Add a variable flagging whether 
       mutate(major_minor_flag = ifelse(total_relevant_area < 20, "minor", "major"))
     all_years <- all_years %>% 
       bind_rows(single_year)
 }
 
+#Summarise the key stats for each year and major/minor group
 all_years_summary <- all_years %>% 
   group_by(cropyear, major_minor_flag) %>% 
   summarise(count=n(),
